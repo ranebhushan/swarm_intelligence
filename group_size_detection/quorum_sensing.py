8d3a679291d9542aab53a99f32510b643728a3c7
ranebhushan <bhushanrane13@gmail.com>
2023-02-27 16:21:19 -0500

    Working Code

--- group_size_detection/quorum_sensing.py
+++ group_size_detection/quorum_sensing.py
@@ -8,33 +8,128 @@ import matplotlib.pyplot as plt
 from PIL import Image as PILImage
 import os
 import shutil
+import random
 
-width = 10
-height = 10
+Width = 10
+Height = 10
 
-initiation_probability = 0.1
+P = 0.1
 
-refractory_timer = 10
+R = 5
 
-agents_grid = np.zeros((height,width)) 
+signal = np.zeros((Height,Width)) 
 
 # False = 0
 # True = 1
-initiated_grid = np.zeros((height,width))
+initiated = np.zeros((Height,Width))
 
-size_grid = np.zeros((height,width))
+group_size = np.zeros((Height,Width))
 
 # SUSCEPTIBLE = 0
 # REFACTORY = 1
-state_grid = np.zeros((height,width))
+agent_state = np.zeros((Height,Width))
 
-# Direction Vectors to explorte neighbours of any agent
-dR = [-1, +1, 0, 0]
-dC = [0, 0, +1, -1]
+agent_done = np.zeros((Height,Width))
 
-for row in range(0,agents_grid.shape[0]):
-    for column in range(0,agents_grid.shape[1]):
-        if(state_grid[row,column] == 0):
-            for i in range(0,len(dR)):
-                new_row = row + dR[i]
-                new_column = column + dC[i]
+refractory_timer = np.zeros((Height,Width))
+
+# Direction Vectors to explore neighbours of any agent
+# 4 Neighbors
+dR = [-1, 0, +1, 0]
+dC = [0, +1, 0, -1]
+
+# 8 Neighbors
+# dR = [-1, -1, 0, +1, +1, +1, 0, -1]
+# dC = [0, +1, +1, +1, 0, -1, -1, -1]
+
+# Create temporary directory to save images to create a GIF
+folder_name = "temp_data"
+if os.path.exists(folder_name):
+    shutil.rmtree(folder_name)
+    os.mkdir(folder_name)
+else:
+    os.mkdir(folder_name)
+
+# Variables to create a GIF 
+image_count = 1
+filenames = []
+
+# Initialize a counter to repeat 'n' number of times
+bean_counter = 0
+
+while (np.sum(initiated) < (Height*Width)):
+
+    for row in range(0,Height):
+        for column in range(0,Width):
+            print(f'Counter = {bean_counter}, Max. Group Size = {np.amax(group_size)}, Group Size Sum = {np.sum(group_size)}', end='\r')
+
+            # Check whether state is susceptible
+            if(agent_state[row,column] == 0):
+
+                # Explore neighbors
+                for i in range(0,len(dR)):
+                    new_row = row + dR[i]
+                    new_column = column + dC[i]
+
+                    # Check if the neighbours are valid or not 
+                    if (new_row < 0 or new_column < 0):
+                        # print("Array index negative")
+                        continue
+
+                    if (new_row >= agent_state.shape[0] or new_column >= agent_state.shape[1]):
+                        # print("Array out of bounds")
+                        continue
+
+                    # Check if the neighbor signalled
+                    if (signal[new_row,new_column] == 1):
+                        signal[row,column] = 1
+                        agent_state[row,column] = 1
+                        refractory_timer[row,column] = R
+                        group_size[row,column] += 1
+                        break
+
+                    elif ((initiated[row,column] == 0) and (random.random() < P)):
+                        signal[row,column] = 1
+                        agent_state[row,column] = 1
+                        refractory_timer[row,column] = R
+                        initiated[row,column] = 1
+                        group_size[row,column] += 1
+                        break
+                        
+                    else:
+                        pass
+
+            else:
+                signal[row,column] = 0
+                refractory_timer[row,column] -= 1
+                if (refractory_timer[row,column] <= 0):
+                    agent_state[row,column] = 0
+                    refractory_timer[row,column] = 0
+    
+    bean_counter += 1
+
+    # Store sequential images in the loop for GIF  
+    filename = f'temp_data/{image_count}.png'
+    filenames.append(filename)
+    image_count = image_count + 1
+    plt.title(f"Group Size Detection")
+    plt.xlabel("Columns")
+    plt.ylabel("Rows")
+    plt.imshow(signal)
+    plt.savefig(filename,bbox_inches='tight')
+    plt.close()
+
+print(f'Counter = {bean_counter}, Max. Group Size = {np.amax(group_size)}, Group Size Sum = {np.sum(group_size)}')
+
+# Read and Open the saved images 
+images = []
+for n in filenames:
+    frame = PILImage.open(n)
+    images.append(frame)
+
+# Create GIF
+images[0].save(f'signal.gif',
+            save_all=True,
+            append_images=images[1:],
+            duration=10,
+            loop=0)
